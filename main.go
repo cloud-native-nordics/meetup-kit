279f4662a086723949b402d463204afae2923810
Jeppe Johansen <jj@whyyy.dk>
2019-10-28 16:14:19 +0100

    Corrected link for master merge

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 )
 
 var port = flag.String("port", "8080", "Application port to use")
-var statsURL = flag.String("stats-url", "https://raw.githubusercontent.com/cloud-native-nordics/meetups/improve_generator_phase5/config.json", "Location of the stats file")
+var statsURL = flag.String("stats-url", "https://raw.githubusercontent.com/cloud-native-nordics/meetups/master/config.json", "Location of the stats file")
 var slackToken = flag.String("slack-token", "", "Slack token to produce invites")
 var slackURL = flag.String("slack-url", "https://cloud-native-nordics.slack.com", "URL to the slack community")
 var slackCommunity = flag.String("slack-community", "Cloud Native Nordicsn", "Name of the slack community")
