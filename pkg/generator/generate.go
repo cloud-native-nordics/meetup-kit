19ef7a1fae33b2754bd81bd34b3ec351c999acbc
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2020-01-26 21:34:08 +0200

    Merge main.go and the placeholder generator.go into generate.go

--- pkg/generator/generate.go
+++ pkg/generator/generate.go
@@ -15,6 +15,26 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
+// Options for the generator
+// TODO: Make this better :)
+type Options struct {
+	// SpeakersFile points to the speakers.yaml file
+	SpeakersFile string
+	// CompaniesFile points to the companies.yaml file
+	CompaniesFile string
+	// RootDir points to the directory that has all meetup groups as subfolders, each with a meetup.yaml file
+	RootDir string
+	// DryRun controls whether to actually apply the changes or not
+	DryRun bool
+	// Validate controls whether to validate the current state of the repo content with the spec
+	Validate bool
+}
+
+func Generate(opts *Options) error {
+	fmt.Println("Generating... no-op at the moment!")
+	return nil
+}
+
 var speakersFile = pflag.String("speakers-file", "speakers.yaml", "Point to the speakers.yaml file")
 var companiesFile = pflag.String("companies-file", "companies.yaml", "Point to the companies.yaml file")
 var rootDir = pflag.String("meetups-dir", ".", "Point to the directory that has all meetup groups as subfolders, each with a meetup.yaml file")
